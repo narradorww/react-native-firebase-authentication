28439d6bcab17adedc944c7e35cba4dd0e84d735
narradorww <rodrigo.anst@gmail.com>
2023-01-30 22:57:34 -0300

    Criada a conexao com o firebase e logica de cadastro

--- src/servicos/requisicoesFirebase.js
+++ src/servicos/requisicoesFirebase.js
@@ -1,12 +1,33 @@
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
-export async function cadastrar(email, senha, confirmaSenha) {
-  createUserWithEmailAndPassword(auth, email, senha)
+
+function errosFirebase(error){
+  switch (error.code) {
+    case AuthErrorCodes.INVALID_EMAIL:
+      return 'E-mail inválido';
+    case AuthErrorCodes.EMAIL_ALREADY_IN_USE:
+      return 'E-mail já cadastrado';
+    case AuthErrorCodes.WEAK_PASSWORD:
+      return 'Senha fraca';
+    default:
+      return 'Erro desconhecido';
+  }
+}
+
+
+
+export async function cadastrar(email, senha) {
+
+  const resultado = await createUserWithEmailAndPassword(auth, email, senha)
   .then((dadosDoUsuario) => {
     console.log(dadosDoUsuario)
+    return
   })
   .catch((error) => {
     console.log(error)
+    return errosFirebase(error);
   });
+
+  return resultado;
 }
--- src/telas/Cadastro/index.js
+++ src/telas/Cadastro/index.js
@@ -1,44 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import Botao from '../../componentes/Botao';
-import { EntradaTexto } from '../../componentes/EntradaTexto';
-import estilos from './estilos';
-import { cadastrar } from '../../servicos/requisicoesFirebase';
+import React, { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+import Botao from "../../componentes/Botao";
+import { EntradaTexto } from "../../componentes/EntradaTexto";
+import estilos from "./estilos";
+import { cadastrar } from "../../servicos/requisicoesFirebase";
+
+export default function Cadastro({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
+  const [statusError, setStatusError] = useState("");
+  const [messageError, setMessageError] = useState("");
+
+  async function realizarCadastro() {
+    if (email === "") {
+      setMessageError("O campo e-mail é obrigatório");
+      setStatusError("email");
+    } else if (senha === "") {
+      setMessageError("O campo senha é obrigatório");
+      setStatusError("senha");
+    } else if (confirmaSenha === "") {
+      setMessageError("O campo confirmar senha é obrigatório");
+      setStatusError("confirmaSenha");
+    } else if(senha != confirmaSenha){
+      setMessageError('As senhas não conferem');
+      setStatusError('confirmaSenha');
+        } else {
+
+        
 
 
-export default function Cadastro({ navigation }) {  
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confirmaSenha, setConfirmaSenha] = useState('');
-
-  async function realizarCadastro(){
-    await cadastrar(email, senha, confirmaSenha);
-    setEmail('')
-    setSenha('')
-    setConfirmaSenha('')
+    const resultado = await cadastrar(email, senha);
+    if(resultado=='sucesso'){
+      Alert.alert('Cadastro realizado com sucesso');
+      setEmail("");
+      setSenha("");
+      setConfirmaSenha("");
+      setStatusError("");
+      setMessageError("");
+    }
+    else{
+      Alert.alert(resultado);
+    }
+    
   }
+} 
 
   return (
     <View style={estilos.container}>
-      <EntradaTexto 
+      <EntradaTexto
         label="E-mail"
         value={email}
-        onChangeText={texto => setEmail(texto)}
+        onChangeText={(texto) => setEmail(texto)}
+        error={(statusError == "email")}
+        messageError={messageError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
-        onChangeText={texto => setSenha(texto)}
+        onChangeText={(texto) => setSenha(texto)}
         secureTextEntry
+        error={(statusError == "senha")}
+        messageError={messageError}
       />
 
       <EntradaTexto
         label="Confirmar Senha"
         value={confirmaSenha}
-        onChangeText={texto => setConfirmaSenha(texto)}
+        onChangeText={(texto) => setConfirmaSenha(texto)}
         secureTextEntry
+        error={statusError == "confirmaSenha"}
+        messageError={messageError}
       />
-      
+
       <Botao onPress={() => realizarCadastro()}>CADASTRAR</Botao>
     </View>
   );
